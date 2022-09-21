45950eb274495aba9a1590e1653b2c10392bde65
İdil Öziş <idil.ozis@ug.bilkent.edu.tr>
2022-09-21 12:26:27 +0300

    Methods to create an array and find max and min numbers in it

--- TeamAssignement_HW2.java
+++ TeamAssignement_HW2.java
@@ -1,4 +1,47 @@
 public class TeamAssignement_HW2 {
+    public class Array{
+    
+    public static int[] createAndFill(int arraySize){
+        
+        int[] arr = new int[arraySize];
+        int index = 0;
+        
+        while(index < arraySize){
+            arr[index] = (int)Math.round(Math.random() * 100);
+            index++;
+        }
+
+        return arr;
+
+    }
+
+    public static int findMax(int arr[]){
+
+        int max = arr[0];
+
+        for(int i = 1; i < arr.length; i++){
+            if(max < arr[i]){
+                max = arr[i];
+            }
+        }
+        return max;
+        
+    }
+
+    public static int findMin(int arr[]){
+
+        int min = arr[0];
+
+        for(int i = 1; i < arr.length; i++){
+            if(min > arr[i]){
+                min = arr[i];
+            }
+        }
+        return min;
+        
+    }
+
+}
     public static void main(String[] args) {
         
     }
