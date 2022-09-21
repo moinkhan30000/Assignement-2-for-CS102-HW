5121cd01448b4dd90f694e6e3ab09c628204ab51
Moin Khan <moin.khan@ug.bilkent.edu.tr>
2022-09-21 12:27:06 +0300

    Added the method to find differences from the average of an array

--- TeamAssignement_HW2.java
+++ TeamAssignement_HW2.java
@@ -1,4 +1,29 @@
 public class TeamAssignement_HW2 {
+    public static double[] differenceOfAverageOfArray(int s[])
+    {
+        int sizeOfArray = s.length;
+        double sumOfArray = 0 ;
+        double averageOfArray;
+        for(int i=0; i<sizeOfArray; i++)
+        {
+            sumOfArray = sumOfArray + s[i];
+        }
+        averageOfArray = sumOfArray/sizeOfArray;
+        double s2[] = new double[sizeOfArray];
+        for (int j = 0; j<sizeOfArray; j++)
+        {
+            double difference;
+            difference = s[j] - averageOfArray;
+            s2[j] = difference;
+        }
+        return s2;
+    }
+    public static void main(String[] args) {
+        int q[] = {2, 6, 1, 3, 5};
+        double r[] = averageOfArray(q);
+        System.out.println(Arrays.toString(r));;
+    }
+}
     public static void main(String[] args) {
         
     }
